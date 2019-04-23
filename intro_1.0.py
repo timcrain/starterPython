777d531013d71fd530d32dba669376c30214a031
Tim Crain <tim@intuitivemachines.com>
2019-04-23 15:05:04 -0500

    Added simple "Hello world" script

---
+++ intro_1.0.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 08:48:17 2019
+
+@author: Tim Crain
+"""
+
+"""
+-----------------------------------------------------------------------------
+"""
+if __name__ == "__main__":
+    
+    
+    aString = "Hello world!"
+
+    print aString
+    
