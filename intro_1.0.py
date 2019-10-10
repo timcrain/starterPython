5596dd577b91a920a6e8fab3a3adaba41afcc44b
timcrain <tim@intuitivemachines.com>
2019-10-09 20:16:13 -0500

    Simple adds for first demonstration

--- intro_1.0.py
+++ intro_1.0.py
@@ -1,17 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 14 08:48:17 2019
+# C1 - Single line comments in python begin with the hash tag
 
-@author: Tim Crain
+"""
+C2 - Multi-line comments begin with 3 double quotes "
+and end with 3 double quotes
+"""
+
+"""
+C3 One thing to know about Python is that it is very sensitive to how much
+indention you put on a line.   For example,
+
+This is way different
+    From this...I'll explain how later
 """
 
 """
 -----------------------------------------------------------------------------
 """
-if __name__ == "__main__":
-    
-    
-    aString = "Hello world!"
 
-    print aString
-    
+# you can create variables in Python just by initializing them as follows:
+number_of_apples = 4
+
+# now the variable number_of_apples has the value 4
+# you can see this simply by using print:
+print number_of_apples
+
+# you can also get fancy like this:
+print "The number of apples is...ta-da!!!! " + str(number_of_apples)
+
+# try executing this file in your Python interpreter
+# I used the mac instructions here:
+# https://blog.usejournal.com/python-with-visual-studio-code-on-macos-60e1fad9e932
+#
+# Love, Dad
---
+++ intro_1.1.py
@@ -0,0 +1,10 @@
+
+
+
+if __name__ == "__main__":
+    
+    
+    aString = "Hello world!"
+
+    print aString
+    
